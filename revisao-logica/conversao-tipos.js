5d1594ebadbc1a7c4e8af950ab593eb7324ee5f6
Davi Augusto <daviaugusto.developer@gmail.com>
2021-01-19 22:11:00 -0300

    conversao

---
+++ revisao-logica/conversao-tipos.js
@@ -0,0 +1,14 @@
+let n1 = "10"
+// n1 = Number(n1)
+n1 = Number(n1)
+let n2 = 2
+
+console.log(n1 + n2, typeof n1, typeof n2)
+
+// parseFloat, parseInt, Number()
+
+n2 = 12
+
+n2 = n2.toString(2)
+
+console.log(n2, typeof n2)
