dcbf617609959a6d59b82b7769fee7c4a54a6e9f
Davi Augusto <daviaugusto.developer@gmail.com>
2021-01-19 22:15:01 -0300

    referencia vs valor

---
+++ revisao-logica/referencia-vs-valor.js
@@ -0,0 +1,17 @@
+function alteraArray(a) {
+  a.push('adicionado')
+}
+
+let arr = ["a"]
+console.log(arr)
+alteraArray(arr)
+console.log(arr)
+
+function alterPrimitivo(p) {
+  p =+ "adionado"
+}
+
+let msg = "mensagem"
+console.log(msg)
+alterPrimitivo(msg)
+console.log(msg)
