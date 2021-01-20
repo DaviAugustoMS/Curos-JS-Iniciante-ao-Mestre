159448e07d7745dc28df5efefac6d23670dc779b
Davi Augusto <daviaugusto.developer@gmail.com>
2021-01-19 22:16:07 -0300

    variaves

---
+++ revisao-logica/variaves.js
@@ -0,0 +1,21 @@
+// var, let, const
+
+// var -> es5 - cross-browser 
+
+// let e const -> es2015
+
+let teste = 'minha string'
+teste = 10
+
+const teste2 = 10
+
+let teste3
+
+
+teste3 = 20
+console.log(teste3)
+
+const teste4 = 'eu sou imutavel'
+
+console.log(teste4)
+
