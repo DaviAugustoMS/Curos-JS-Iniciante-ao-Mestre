4cd6f5ee736be883b808f52b9dd46dfca8e7a953
Davi Augusto <daviaugusto.developer@gmail.com>
2021-01-19 22:20:46 -0300

    finally

--- revisao-logica/sum.js
+++ revisao-logica/sum.js
@@ -4,13 +4,3 @@ function sum(n1, n2) {
   }
   return n1 + n2
 }
-
-let soma = ""
-try {
-  soma = sum(3, "a")
-} catch (error) {
-  console.log("Ocorreu um erro")
-  console.log(error.message)
-}
-// let soma = sum(3, "a")
-console.log(soma)
