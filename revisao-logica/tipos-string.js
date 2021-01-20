b9d0dd5b771bcd9f5a6b198448a4df59e3cf9906
Davi Augusto <daviaugusto.developer@gmail.com>
2021-01-19 22:15:43 -0300

    types string

---
+++ revisao-logica/tipos-string.js
@@ -0,0 +1,11 @@
+// string, number, (int, floats), boolean
+
+let minhaVar = 'minha string'
+let minhavar2 = "minha \"string\""
+var minhavar3 = `minha templade`
+
+let idade = 40
+let msg = `eu possuo ${idade} anos`
+console.log(msg)
+
+console.log(typeof msg, typeof idade, typeof minhaVar)
