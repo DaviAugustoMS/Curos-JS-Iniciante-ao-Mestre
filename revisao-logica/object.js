8fc0b164971d28158930cd2bd3aa50a33ea5b573
Davi Augusto <daviaugusto.developer@gmail.com>
2021-01-19 22:14:06 -0300

    object

---
+++ revisao-logica/object.js
@@ -0,0 +1,20 @@
+// const nomes = ['davi', 'lilia', 'enzo']
+
+// const idades = [26, 26, 4]
+
+// console.log(nomes[0], idades[0])
+
+// const pessoa = new Object()
+// pessoa.nome = "davi"
+// pessoa.idade = 26
+// console.log(pessoa)
+// let prop = 'nome'
+// console.log(pessoa[prop])
+// console.log(pessoa.idade)
+
+const pessoa = {
+  nome: "Davi",
+  idade: 26
+}
+
+console.log(pessoa)
